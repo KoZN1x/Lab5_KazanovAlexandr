d460c446d4fb06439540a4b04d9e3f663d049059
Alexandr Kazanov <kazanov.aleks@gmail.com>
2023-01-16 17:30:45 +0300

    Event "Overflowing" commit

---
+++ Hospital/Hospital.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+
+    delegate void Delegate(string s);
+    internal class Hospital : INotifyable
+    {
+        public event Action<Patient> overflow;
+        private List<Patient> patients = new List<Patient>();
+        public void AddPatient(Patient patient)
+        {
+            if (IsOverflowed())
+            {
+                overflow?.Invoke(patient);
+            }
+            else
+            {
+                patients.Add(patient);
+            }
+        }
+        public void RemovePatient(Patient patient)
+        {
+            patients.Remove(patient);
+        }
+        public void ChangeDiagnosis(Patient patient, string diagnosis)
+        {
+            foreach (Patient pat in patients)
+            {
+                if (patient == pat)
+                {
+                    pat.SetDiagnosis(diagnosis);
+                }
+            }
+        }
+        public override string ToString()
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (Patient pat in patients)
+            {
+                stringBuilder.AppendLine(pat.GetPatientInfo());
+            }
+            return stringBuilder.ToString();
+        }
+        private bool IsOverflowed()
+        {
+            if (patients.Count >= 15)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public void Notify(Patient patient)
+        {
+            Console.WriteLine($"Hospital is overflowed! Can't add patient: {patient.GetPatientInfo()}");
+        }
+    }
+}
---
+++ Hospital/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Delegate View = x => Console.WriteLine(x);
+
+            var alex = new Patient("Alex", 45, "Heart attack");
+            var dima = new Patient("Dima", 59, "Stroke");
+            var misha = new Patient("Misha", 27, "Healthy");
+            var alexandr = new Patient("Alexandr", 34, "Heart attack");
+            var aleksey = new Patient("Aleksey", 59, "Stroke");
+            var mihail = new Patient("Mihail", 27, "Healthy");
+            var andrey = new Patient("Andrey", 45, "Heart attack");
+            var roman = new Patient("Roman", 59, "Stroke");
+            var anton = new Patient("Anton", 27, "Healthy");
+            var kirill = new Patient("Kirill", 45, "Heart attack");
+            var matvey = new Patient("Matvey", 59, "Stroke");
+            var yuriy = new Patient("Yuriy", 27, "Healthy");
+            var maksim = new Patient("Maksim", 45, "Heart attack");
+            var anna = new Patient("Anna", 59, "Stroke");
+            var anzhela = new Patient("Anzhela", 27, "Healthy");
+            var nastya = new Patient("Nastya", 45, "Heart attack");
+            var dmitryi = new Patient("Dmitriy", 59, "Stroke");
+            var ivan = new Patient("Ivan", 27, "Healthy");
+
+            var hospital = new Hospital();
+            hospital.overflow += hospital.Notify;
+
+            hospital.AddPatient(alex);
+            hospital.AddPatient(dima);
+            hospital.AddPatient(misha);
+            hospital.AddPatient(alexandr);
+            hospital.AddPatient(aleksey);
+            hospital.AddPatient(mihail);
+            hospital.AddPatient(andrey);
+            hospital.AddPatient(roman);
+            hospital.AddPatient(anton);
+            hospital.AddPatient(kirill);
+            hospital.AddPatient(matvey);
+            hospital.AddPatient(yuriy);
+            hospital.AddPatient(maksim);
+            hospital.AddPatient(anna);
+            hospital.AddPatient(anzhela);
+            hospital.AddPatient(nastya);
+            hospital.AddPatient(dmitryi);
+            hospital.AddPatient(ivan);
+
+
+            View(hospital.ToString());
+            //Console.WriteLine(hospital.ToString());
+
+            hospital.ChangeDiagnosis(alex, "Healthy");
+            View(hospital.ToString());
+            //Console.WriteLine(hospital.ToString());
+
+            hospital.RemovePatient(dima);
+            View(hospital.ToString());
+            //Console.WriteLine(hospital.ToString());
+        }
+    }
+}
